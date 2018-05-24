9ce0f3a8ca7860b85a25ab87aa83c5a71db1a2d4
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2018-05-24 09:48:08 -0300

    fix(webpack): Add postcss, precss, cssnext back. change output file name to styles.css

--- webpack/prod/webpack.prod.config.js
+++ webpack/prod/webpack.prod.config.js
@@ -4,31 +4,53 @@ const {version} = require('../../package');
 
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const precss = require('precss');
+const postcssCssnext = require('postcss-cssnext');
 
 const DIR_ROOT = path.join(__dirname, '../../');
 const DIR_SOURCE = path.resolve(DIR_ROOT, 'src') + '/';
 const DIR_PUBLIC = path.resolve(DIR_ROOT, 'dist') + '/';
+
+console.log('Building dist directory ... \n===========================\n');
 
 module.exports = {
   entry: [ DIR_SOURCE + 'css/app.scss'],
   output: {
     path: DIR_PUBLIC,
     publicPath: DIR_PUBLIC,
-    filename: 'styles.css',
+    filename: 'main.js',
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader']),
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              plugins() {
+                return [
+                  precss,
+                  postcssCssnext,
+                ];
+           },
+          }
+        },
+        'sass-loader'
+        ],
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin({
-       filename: 'css/styles.css',
-       allChunks: true,
-    }),
+    new MiniCssExtractPlugin({
+          filename: "styles.css",
+          chunkFilename: "[id].css"
+        }),
     // Copy files from /src to /public
     new CopyWebpackPlugin([
       { from: DIR_SOURCE + 'img/', to: DIR_PUBLIC + 'img/' },
