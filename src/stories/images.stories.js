ca01085cf85a6f80d7eaaad7476ee22c7871c760
skkr <nico.sikker@gmail.com>
2018-05-31 11:49:51 -0300

    Added image stories and documentation.

---
+++ src/stories/images.stories.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+// Included addons
+import { doc } from 'storybook-readme';
+
+// Included components
+import Resizable from 're-resizable';
+
+// Readme files
+import ResponsiveImages from './images/responsive-images.md';
+import ImageThumbnails from './images/image-thumbnails.md';
+import Picture from './images/picture.md';
+
+
+// COMPONENTS
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+
+// HOCs
+import { withCleanPreview, withCodePreview, } from './HOCs/with-docs'
+
+
+storiesOf('Images', module)
+
+  // Responsive Images
+  .add('Responsive Images', withCleanPreview(ResponsiveImages, () =>
+    <Resizable
+      className="resizable"
+      defaultSize={{ width: 'auto', height: 'auto' }}
+      bounds={'parent'}
+      enable={{ top:false, right:true, bottom:false, left:false, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false }}
+    >
+      <img src="http://via.placeholder.com/895x250" className="img-fluid" alt="Responsive image"/>
+    </Resizable>
+  ))
+
+  // Image Thumbnails
+  .add('Image Thumbnails', withCleanPreview(ImageThumbnails, () =>
+    <img src="http://via.placeholder.com/200x200" alt="Thumbnail" class="img-thumbnail"/>
+  ))
+
+  // Picture
+  .add('Picture', doc(Picture))
---
+++ src/stories/images/responsive-images.md
@@ -0,0 +1,9 @@
+# Responsive images
+
+Images in are made responsive with `.img-fluid`. Simply add that class to an image tag and it will scale with the parent element.
+
+<!-- STORY -->
+
+```html
+<img src="..." class="img-fluid" alt="Responsive image">
+```
