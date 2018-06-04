62438d9c2ba2d5e80639027afa791c54cb94f5b9
skkr <nico.sikker@gmail.com>
2018-06-04 12:53:46 -0300

    Updated media assets to use images from WT media repo.

--- src/stories/branding.stories.js
+++ src/stories/branding.stories.js
@@ -14,6 +14,13 @@ import Colors from './branding/colors.md';
 import Icons from './branding/icons.md';
 import MediaAssets from './branding/media-assets.md';
 
+// Images
+import imgLogo from '../../node_modules/windingtree-media/logo-variants/full-logo/png/logo--gradient-on-white.png';
+import imgLogoAlt from '../../node_modules/windingtree-media/logo-variants/full-logo/png/logo--black-on-green.png';
+import imgTree from '../../node_modules/windingtree-media/logo-variants/tree/png/tree--gradient-on-white.png';
+import imgTreeAlt from '../../node_modules/windingtree-media/logo-variants/tree/png/tree--black-on-white.png';
+import imgSign from '../../node_modules/windingtree-media/logo-variants/sign/png/sign--gradient-on-white.png';
+import imgSignAlt from '../../node_modules/windingtree-media/logo-variants/sign/png/sign--black-on-green.png';
 
 // COMPONENTS
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -125,14 +132,18 @@ storiesOf('Branding', module)
 ))
 
 .add('Media assets', withCleanPreview(MediaAssets, () =>
-  <div className="text-center">
-      <p className="mb-4"> <img src="../img/content/logo.svg" alt="Winding Tree" width="600" className="img-fluid"/> </p>
-      <p className="mb-4"> <img src="../img/content/logo_black.png" alt="Winding Tree" width="700" className="img-fluid"/> </p>
-      <p>
-        <img className="mb-4 mr-3 d-inline-block img-fluid" src="../img/content/tree_gradient.png" alt="Winding Tree" width="150"/>
-        <img className="mb-4 mr-3 d-inline-block img-fluid" src="../img/content/Tree_black.png" alt="Winding Tree" width="150"/>
-        <img className="mb-4 mr-3 d-inline-block img-fluid" src="../img/content/sign_gradient.png" alt="Winding Tree" width="60"/>
-        <img className="mb-4 d-inline-block img-fluid" src="../img/content/sign_black.png" alt="Winding Tree" width="60"/>
-      </p>
+  <div className="text-center col">
+    <p className="mb-4">
+      <img src={imgLogo} alt="Winding Tree Logo" width="600" className="img-fluid"/>
+    </p>
+    <p className="mb-4">
+      <img src={imgLogoAlt} alt="Winding Tree Logo Variant" width="600" className="img-fluid"/>
+    </p>
+    <p>
+      <img className="mb-4 mr-3 d-inline-block img-fluid" src={imgTree} alt="Winding Tree Tree" width="150"/>
+      <img className="mb-4 mr-3 d-inline-block img-fluid" src={imgTreeAlt} alt="Winding Tree Tree variant" width="150"/>
+      <img className="mb-4 mr-3 d-inline-block img-fluid" src={imgSign} alt="Winding Tree Sign" width="60"/>
+      <img className="mb-4 d-inline-block img-fluid" src={imgSignAlt} alt="Winding Tree Sign Variant" width="60"/>
+    </p>
   </div>
 ))
