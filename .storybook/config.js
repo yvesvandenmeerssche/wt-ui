6a22a01b807619e96edf347536ee38170f047960
skkr <nico.sikker@gmail.com>
2018-06-13 12:30:25 -0300

    Added utilities to storybook index.

--- .storybook/config.js
+++ .storybook/config.js
@@ -35,6 +35,9 @@ function loadStories() {
   require('../src/stories/images.stories.js');
   require('../src/stories/controls.stories.js');
   require('../src/stories/navigation.stories.js');
+  // require('../src/stories/containers.stories.js'); disabled until merge
+  // require('../src/stories/feedback.stories.js'); disabled until merge
+  require('../src/stories/utilities.stories.js');
 }
 
 configure(loadStories, module);
