9cd7cf5d8ef274fd753eb16d2697bf8c37d85a91
skkr <nico.sikker@gmail.com>
2018-07-26 17:23:59 -0300

    Replaced modal window close button with mdi icon

--- src/stories/containers.stories.js
+++ src/stories/containers.stories.js
@@ -163,7 +163,7 @@ storiesOf('Containers', module)
             <div className="modal-header">
               <h5 className="modal-title">Modal title</h5>
               <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
+                <i className="mdi mdi-close"></i>
               </button>
             </div>
             <div className="modal-body">
