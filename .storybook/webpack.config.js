fbc0c2d250bdad7fd761ebc347b5b6827cadd7ee
Nico Calabrese <nico.sikker@gmail.com>
2018-10-16 22:19:52 -0300

    chore(storybook): Included new wt-ui-assets and updated config to support image paths

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              name: '[name].[ext]',
+              name: '[path][name].[ext]',
               outputPath: 'img/',
               publicPath: 'img/'
             }
