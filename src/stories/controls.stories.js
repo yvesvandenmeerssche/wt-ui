347c781c1a64249cdd9ba8880d715af28892e9fd
Nico Calabrese <nico.sikker@gmail.com>
2018-10-23 20:20:33 -0300

    Updated form stories and docs

--- src/stories/controls.stories.js
+++ src/stories/controls.stories.js
@@ -23,6 +23,10 @@ import ButtonGroup__Nesting from './controls/button-group__nesting.md';
 import ButtonGroup__Vertical from './controls/button-group__vertical.md';
 
 import Form from './controls/form.md';
+import FormValidation from './controls/form__validation.md';
+import FormInput from './controls/form__input.md';
+import FormSelect from './controls/form__select.md';
+import FormCheckRadio from './controls/form__checkboxes-and-radios.md';
 import InputGroup from './controls/input-group.md';
 import InputGroup__Subscription from './controls/input-group__subscribe.md';
 
@@ -37,42 +41,42 @@ storiesOf('Controls/Button', module)
   // Overview
   .add('Overview', withCodePreview(Button, () =>
     <div>
-        <h6 className="mb-1">Primary <span class="text-muted">( .btn-primary )</span></h6>
+        <h6 className="mb-1">Primary <span className="text-muted">( .btn-primary )</span></h6>
         <div className="mb-2 px-1">
           <button type="button" className="mr-1 mr-sm-2 btn btn-primary btn-sm">Small</button>
           <button type="button" className="mr-1 mr-sm-2 btn btn-primary">Default</button>
           <button type="button" className="mr-1 mr-sm-2 btn btn-primary btn-lg">Large</button>
         </div>
 
-        <h6 className="mb-1">Secondary <span class="text-muted">( .btn-outline-light )</span></h6>
+        <h6 className="mb-1">Secondary <span className="text-muted">( .btn-outline-light )</span></h6>
         <div className="bg--grad mb-2 p-1">
           <button type="button" className="mr-1 mr-sm-2 btn btn-outline-light btn-sm">Small</button>
           <button type="button" className="mr-1 mr-sm-2 btn btn-outline-light">Default</button>
           <button type="button" className="mr-1 mr-sm-2 btn btn-outline-light btn-lg">Large</button>
         </div>
 
-        <h6 className="mb-1">Secondary <span class="text-muted">( .btn-outline-dark )</span></h6>
+        <h6 className="mb-1">Secondary <span className="text-muted">( .btn-outline-dark )</span></h6>
         <div className="mb-2 px-1">
           <button type="button" className="mr-1 mr-sm-2 btn btn-outline-dark btn-sm">Small</button>
           <button type="button" className="mr-1 mr-sm-2 btn btn-outline-dark">Default</button>
           <button type="button" className="mr-1 mr-sm-2 btn btn-outline-dark btn-lg">Large</button>
         </div>
 
-        <h6 className="mb-1">Accent <span class="text-muted">( .btn--accent )</span></h6>
+        <h6 className="mb-1">Accent <span className="text-muted">( .btn--accent )</span></h6>
         <div className="mb-2 px-1">
           <button type="button" className="mr-1 mr-sm-2 btn btn--accent btn-sm">Small</button>
           <button type="button" className="mr-1 mr-sm-2 btn btn--accent">Default</button>
           <button type="button" className="mr-1 mr-sm-2 btn btn--accent btn-lg">Large</button>
         </div>
 
-        <h6 className="mb-1">Subtle <span class="text-muted">( .btn-light )</span></h6>
+        <h6 className="mb-1">Subtle <span className="text-muted">( .btn-light )</span></h6>
         <div className="mb-2 px-1">
           <button type="button" className="mr-1 mr-sm-2 btn btn-light btn-sm">Small</button>
           <button type="button" className="mr-1 mr-sm-2 btn btn-light">Default</button>
           <button type="button" className="mr-1 mr-sm-2 btn btn-light btn-lg">Large</button>
         </div>
 
-        <h6 className="mb-1">Minimal <span class="text-muted">( .btn-link )</span></h6>
+        <h6 className="mb-1">Minimal <span className="text-muted">( .btn-link )</span></h6>
         <div className="mb-1 px-1">
           <button type="button" className="mr-1 mr-sm-2 btn btn-link btn-sm">Small</button>
           <button type="button" className="mr-1 mr-sm-2 btn btn-link">Default</button>
@@ -108,7 +112,7 @@ storiesOf('Controls/Button', module)
       <a href="#" className="mr-1 mb-1 btn btn-primary active" role="button" aria-pressed="true">Primary link</a>
       <button type="button" className="mr-1 mb-1 btn btn-primary" disabled>Primary button</button>
       <a href="#" className="mb-1 btn btn-primary disabled" role="button" aria-disabled="true">Primary link</a>
-      <hr className="my-1"/>
+      <hr className="my-2"/>
       <h6 className="mb-1">Create a loading button by adding a loading icon</h6>
       <button className="btn btn-primary disabled" disabled>
         <i className="mdi mdi-loading mdi-spin text-dark"/> Loading...
@@ -212,24 +216,240 @@ storiesOf('Controls/Form', module)
   // Form
   .add('Overview', withCodePreview(Form, () =>
     <form>
+
       <div className="form-group">
-        <label htmlFor="exampleInputEmail1">Email address</label>
-        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+        <label for="exampleInput">Email address</label>
+        <input type="email" id="exampleInput" placeholder="name@example.com"/>
       </div>
+
       <div className="form-group">
-        <label htmlFor="exampleInputPassword1">Password</label>
-        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+        <label for="exampleSelect">Example select</label>
+        <select id="exampleSelect">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+        </select>
       </div>
-      <div className="form-check mb-1">
-        <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+
+      <div className="form-group mb-1">
+        <label for="exampleTextArea">Example textarea</label>
+        <textarea id="exampleTextArea" rows="3"></textarea>
       </div>
+
+      <h6 className="mb-1 text--sm">Checkboxes & Radio buttons</h6>
+
+      <div className="form-group mb-1">
+        <div className="custom-control custom-checkbox">
+          <input type="checkbox" className="custom-control-input" id="customCheck1" />
+          <label className="custom-control-label" for="customCheck1">Option 1</label>
+        </div>
+        <div className="custom-control custom-checkbox mb-1">
+          <input type="checkbox" className="custom-control-input" id="customCheck2"/>
+          <label className="custom-control-label" for="customCheck2">Option 2</label>
+        </div>
+
+        <div className="custom-control custom-checkbox custom-control-inline">
+        <input type="checkbox" className="custom-control-input" id="customInlineCheck2" />
+          <label className="custom-control-label" for="customInlineCheck2">Option 1</label>
+        </div>
+        <div className="custom-control custom-checkbox custom-control-inline mb-1">
+        <input type="checkbox" className="custom-control-input" id="customInlineCheck3"/>
+          <label className="custom-control-label" for="customInlineCheck3">Option 2</label>
+        </div>
+
+        <div className="custom-control custom-radio">
+          <input type="radio" id="customRadio1" name="customRadio" className="custom-control-input" checked/>
+          <label className="custom-control-label" for="customRadio1">Option 1</label>
+        </div>
+        <div className="custom-control custom-radio mb-1">
+          <input type="radio" id="customRadio2" name="customRadio" className="custom-control-input"/>
+          <label className="custom-control-label" for="customRadio2">Option 2</label>
+        </div>
+
+        <div className="custom-control custom-radio custom-control-inline">
+          <input type="radio" id="customRadioInline1" name="customRadioInline1" className="custom-control-input" checked/>
+          <label className="custom-control-label" for="customRadioInline1">Option 1</label>
+        </div>
+        <div className="custom-control custom-radio custom-control-inline">
+          <input type="radio" id="customRadioInline2" name="customRadioInline1" className="custom-control-input"/>
+          <label className="custom-control-label" for="customRadioInline2">Option 2</label>
+        </div>
+      </div>
+
       <button type="submit" className="btn btn-primary">Submit</button>
+
     </form>
   ))
+  // Form
+  .add('Validation', withCodePreview(FormValidation, () =>
+
+  <div>
+    <form>
+      <div class="form-row">
+        <div class="col-md-4 mb-1">
+          <label for="validationDefault01">First name</label>
+          <input type="text" id="validationDefault01" placeholder="First name" value="Mark" required/>
+        </div>
+        <div class="col-md-4 mb-1">
+          <label for="validationDefault02">Last name</label>
+          <input type="text" id="validationDefault02" placeholder="Last name" value="Otto" required/>
+        </div>
+        <div class="col-md-4 mb-1">
+          <label for="validationDefaultUsername">Username</label>
+          <input type="text" id="validationDefaultUsername" placeholder="Username" aria-describedby="inputGroupPrepend2" required/>
+        </div>
+      </div>
+      <button class="btn btn-primary" type="submit">Submit</button>
+    </form>
+
+    <hr className="my-2"/>
+
+    <form>
+      <div className="form-group">
+        <label for="exampleInputValid">Email address</label>
+        <input className="is-valid" type="email" id="exampleInputValid" placeholder="name@example.com"/>
+        <div class="valid-feedback">Looks good!</div>
+      </div>
+
+      <div className="form-group">
+        <label for="exampleInputInvalid">Email address</label>
+        <input className="is-invalid" type="email" id="exampleInputInvalid" placeholder="name@example.com"/>
+        <div class="invalid-feedback">Please enter a valid email address.</div>
+      </div>
 
 
+      <div className="form-group">
+        <label for="exampleSelect">Example select</label>
+        <select id="exampleSelect" className="is-invalid">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+        </select>
+      </div>
+
+      <div className="form-group mb-1">
+        <label for="exampleTextArea">Example textarea</label>
+        <textarea id="exampleTextArea" rows="3" className="is-invalid"></textarea>
+      </div>
+
+      <h6 className="mb-1 text--sm">Checkboxes & Radio buttons</h6>
+
+      <div className="form-group mb-1">
+        <div className="custom-control custom-checkbox">
+          <input type="checkbox" className="custom-control-input is-invalid" id="customCheck1" />
+          <label className="custom-control-label" for="customCheck1">Option 1</label>
+        </div>
+        <div className="custom-control custom-checkbox mb-1">
+          <input type="checkbox" className="custom-control-input" id="customCheck2"/>
+          <label className="custom-control-label" for="customCheck2">Option 2</label>
+        </div>
+
+        <div className="custom-control custom-radio">
+          <input type="radio" id="customRadio1" name="customRadio" className="custom-control-input is-invalid" checked/>
+          <label className="custom-control-label" for="customRadio1">Option 1</label>
+        </div>
+        <div className="custom-control custom-radio mb-1">
+          <input type="radio" id="customRadio2" name="customRadio" className="custom-control-input"/>
+          <label className="custom-control-label" for="customRadio2">Option 2</label>
+        </div>
+      </div>
+
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+    </div>
+  ))
+  // Form Input
+  .add('Input', withCodePreview(FormInput, () =>
+    <form>
+      <div className="form-group">
+        <label htmlFor="defaultInput">Input</label>
+        <input type="text" id="defaultInput" aria-describedby="sampleInput" placeholder="Default input"/>
+        <small id="inputHelp" className="form-text">We'll never share your email with anyone else.</small>
+      </div>
+
+      <div className="form-group mt-1">
+        <input type="text" className="input--sm" aria-describedby="sampleInputSmall" placeholder="Small input"/>
+      </div>
+
+      <div className="form-group">
+        <input type="text" className="input--lg" aria-describedby="sampleInputLarge" placeholder="Large input"/>
+      </div>
+    </form>
+  ))
+  // Form Select
+  .add('Select', withCodePreview(FormSelect, () =>
+    <form>
+      <div className="form-group">
+        <label htmlFor="defaultSelect">Select</label>
+        <select id="defaultSelect">
+          <option selected>Default select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </div>
+
+      <div className="form-group mt-1">
+        <select className="select--sm">
+          <option selected>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </div>
+
+      <div className="form-group">
+        <select className="select--lg">
+          <option selected>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </div>
+    </form>
+  ))
+  // Form Checkboxes and Radios
+  .add('Checkboxes and Radios', withCodePreview(FormCheckRadio, () =>
+    <form>
+      <div className="custom-checkbox">
+        <input type="checkbox" id="checkbox" />
+        <label for="checkbox">Option 1</label>
+      </div>
+      <div className="custom-checkbox mb-1">
+        <input type="checkbox" id="checkbox2"/>
+        <label for="checkbox2">Option 2</label>
+      </div>
+
+      <div className="custom-checkbox custom-checkbox--inline">
+        <input type="checkbox" id="checkbox3" />
+        <label for="checkbox3">Option 1</label>
+      </div>
+      <div className="custom-checkbox custom-checkbox--inline mb-1">
+        <input type="checkbox" id="checkbox4"/>
+        <label for="checkbox4">Option 2</label>
+      </div>
+
+      <h6 className="my-1 text--sm">Radio Buttons</h6>
+
+      <div className="custom-radio">
+        <input type="radio" id="radio" name="radio" checked/>
+        <label for="radio">Option 1</label>
+      </div>
+      <div className="custom-radio mb-1">
+        <input type="radio" id="radio1" name="radio"/>
+        <label for="radio1">Option 2</label>
+      </div>
+
+      <div className="custom-radio custom-radio--inline">
+        <input type="radio" id="radioInline" name="radioInline" checked/>
+        <label for="radioInline">Option 1</label>
+      </div>
+      <div className="custom-radio custom-radio--inline">
+        <input type="radio" id="radioInline1" name="radioInline"/>
+        <label for="radioInline1">Option 2</label>
+      </div>
+    </form>
+  ))
 storiesOf('Controls/Input Group', module)
   // Input Group
   .add('Overview', withCodePreview(InputGroup, () =>
@@ -277,22 +497,60 @@ storiesOf('Controls/Input Group', module)
   // Subscription
   .add('Subscription', withCodePreview(InputGroup__Subscription, () =>
   <div>
-    <form action="#" className="input-group mb-2">
-        <input type="email" className="form-control form-control-lg" placeholder="Enter e-mail"/>
-        <div className="input-group-append">
-          <button className="btn btn-block btn-lg btn-primary" type="submit">
-            Subscribe
-          </button>
-        </div>
+    <form action="#" className="input-group mb-1">
+      <input type="email" className="form-control form-control-sm" placeholder="Enter e-mail"/>
+      <div className="input-group-append">
+        <button className="btn btn-block btn-sm btn-primary" type="submit">
+          Subscribe
+        </button>
+      </div>
     </form>
 
-    <p className="mb-2">If you want to make the component responsive add the <code>.input-group--responsive</code> modifier,
-    the text field and the button will be displayed one on top of the other on small viewports.</p>
+    <form action="#" className="input-group mb-1">
+      <input type="email" className="form-control" placeholder="Enter e-mail"/>
+      <div className="input-group-append">
+        <button className="btn btn-block btn-primary" type="submit">
+          Subscribe
+        </button>
+      </div>
+    </form>
 
-    <form action="#" className="input-group input-group--responsive">
+    <form action="#" className="input-group mb-1">
       <input type="email" className="form-control form-control-lg" placeholder="Enter e-mail"/>
       <div className="input-group-append">
         <button className="btn btn-block btn-lg btn-primary" type="submit">
+          Subscribe
+        </button>
+      </div>
+    </form>
+
+    <h5 className="mt-2">Responsive version</h5>
+
+    <p className="my-1">If you want to make the component responsive add the <code>.input-group--responsive</code> modifier,
+    the text field and the button will be displayed one on top of the other on small viewports.</p>
+
+    <form action="#" className="input-group input-group--responsive mb-1">
+      <input type="email" className="form-control form-control-sm" placeholder="Enter e-mail"/>
+      <div className="input-group-append">
+        <button className="btn btn-block btn-primary btn-sm" type="submit">
+          Subscribe
+        </button>
+      </div>
+    </form>
+
+    <form action="#" className="input-group input-group--responsive mb-1">
+      <input type="email" className="form-control" placeholder="Enter e-mail"/>
+      <div className="input-group-append">
+        <button className="btn btn-block btn-primary" type="submit">
+          Subscribe
+        </button>
+      </div>
+    </form>
+
+    <form action="#" className="input-group input-group--responsive mb-1">
+      <input type="email" className="form-control form-control-lg" placeholder="Enter e-mail"/>
+      <div className="input-group-append">
+        <button className="btn btn-block btn-primary btn-lg" type="submit">
           Subscribe
         </button>
       </div>
--- src/stories/controls/form.md
+++ src/stories/controls/form.md
@@ -1,88 +1,79 @@
 # Form
 
-Use the form classes to opt into customized displays for a more consistent rendering across browsers and devices.
-
-Be sure to use an appropriate `type` attribute on all inputs (e.g., `email` for email address or `number` for numerical information) to take advantage of newer input controls like email verification, number selection, and more.
-
-Here’s a quick example to demonstrate form's styles.
-
-Refer to [Bootstrap's documentation](https://getbootstrap.com/docs/4.0/components/forms/#overview) to learn more on required classes, form layout, and more.
+The `.form-group` class is the easiest way to add some structure to forms. It provides a flexible class that encourages proper grouping of labels, controls, optional help text, and form validation messaging. By default it only applies `margin-bottom`, but it picks up additional styles in `.form-inline` as needed. Use it with `<fieldset>`s, `<div>`s, or nearly any other element.
 
 <!-- STORY -->
 
 ```html
 <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  <!-- Input -->
+  <div className="form-group">
+    <label for="exampleInput">Email address</label>
+    <input type="email" id="exampleInput" placeholder="name@example.com"/>
   </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+
+  <!-- Select -->
+  <div className="form-group">
+    <label for="exampleSelect">Example select</label>
+    <select id="exampleSelect">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+    </select>
   </div>
-  <div class="form-check mb-1">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+
+  <!-- Textarea -->
+  <div className="form-group">
+    <label for="exampleTextArea">Example textarea</label>
+    <textarea id="exampleTextArea" rows="3"></textarea>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+
+  <!-- Checkboxes & Radios-->
+  <div className="form-group">
+
+  <!-- Checkboxes -->
+    <div className="custom-checkbox">
+      <input type="checkbox" id="checkbox" />
+      <label for="checkbox">Option 1</label>
+    </div>
+    <div className="custom-checkbox">
+      <input type="checkbox" id="checkbox2"/>
+      <label for="checkbox2">Option 2</label>
+    </div>
+
+    <!-- Inline Checkboxes -->
+    <div className="custom-checkbox custom-checkbox--inline">
+      <input type="checkbox" id="checkbox3" />
+      <label for="checkbox3">Option 1</label>
+    </div>
+    <div className="custom-checkbox custom-checkbox--inline">
+      <input type="checkbox" id="checkbox4"/>
+      <label for="checkbox4">Option 2</label>
+    </div>
+
+    <h6 className="my-1 text--sm">Radio Buttons</h6>
+
+    <!-- Radio buttons -->
+    <div className="custom-radio">
+      <input type="radio" id="radio" name="radio" checked/>
+      <label for="radio">Option 1</label>
+    </div>
+    <div className="custom-radio">
+      <input type="radio" id="radio1" name="radio"/>
+      <label for="radio1">Option 2</label>
+    </div>
+
+    <!-- Inline Radio buttons -->
+    <div className="custom-radio custom-radio--inline">
+      <input type="radio" id="radioInline" name="radioInline" checked/>
+      <label for="radioInline">Option 1</label>
+    </div>
+    <div className="custom-radio custom-radio--inline">
+      <input type="radio" id="radioInline1" name="radioInline"/>
+      <label for="radioInline1">Option 2</label>
+    </div>
+  </div>
+
+  <button type="submit" className="btn btn-primary">Submit</button>
 </form>
 ```
-
-## Also check
-
-Refer to Bootstrap's documentation for more information.
-
-- [Form controls][0]
-  - [Sizing][1]
-  - [Readonly][2]
-  - [Readonly plain text][3]
-- [Checkboxes and radios][4]
-  - [Default (stacked)][5]
-  - [Inline][6]
-  - [Without labels][7]
-- [Layout][8]
-  - [Form groups][9]
-  - [Form grid][10]
-  - [Inline forms][11]
-- [Help text][12]
-- [Disabled forms][13]
-  - [Cross-browser compatibility][14]
-- [Validation][15]
-  - [How it works][16]
-  - [Custom styles][17]
-  - [Browser defaults][18]
-  - [Server side][19]
-  - [Supported elements][20]
-  - [Tooltips][21]
-- [Custom forms][22]
-  - [Checkboxes and radios][23]
-  - [Select menu][24]
-  - [File browser][25]
-
-[0]: https://getbootstrap.com/docs/4.0/components/forms/#form-controls
-[1]: https://getbootstrap.com/docs/4.0/components/forms/#sizing
-[2]: https://getbootstrap.com/docs/4.0/components/forms/#readonly
-[3]: https://getbootstrap.com/docs/4.0/components/forms/#readonly-plain-text
-[4]: https://getbootstrap.com/docs/4.0/components/forms/#checkboxes-and-radios
-[5]: https://getbootstrap.com/docs/4.0/components/forms/#default-stacked
-[6]: https://getbootstrap.com/docs/4.0/components/forms/#inline
-[7]: https://getbootstrap.com/docs/4.0/components/forms/#without-labels
-[8]: https://getbootstrap.com/docs/4.0/components/forms/#layout
-[9]: https://getbootstrap.com/docs/4.0/components/forms/#form-groups
-[10]: https://getbootstrap.com/docs/4.0/components/forms/#form-grid
-[11]: https://getbootstrap.com/docs/4.0/components/forms/#inline-forms
-[12]: https://getbootstrap.com/docs/4.0/components/forms/#help-text
-[13]: https://getbootstrap.com/docs/4.0/components/forms/#disabled-forms
-[14]: https://getbootstrap.com/docs/4.0/components/forms/#cross-browser-compatibility
-[15]: https://getbootstrap.com/docs/4.0/components/forms/#validation
-[16]: https://getbootstrap.com/docs/4.0/components/forms/#how-it-works
-[17]: https://getbootstrap.com/docs/4.0/components/forms/#custom-styles
-[18]: https://getbootstrap.com/docs/4.0/components/forms/#browser-defaults
-[19]: https://getbootstrap.com/docs/4.0/components/forms/#server-side
-[20]: https://getbootstrap.com/docs/4.0/components/forms/#supported-elements
-[21]: https://getbootstrap.com/docs/4.0/components/forms/#tooltips
-[22]: https://getbootstrap.com/docs/4.0/components/forms/#custom-forms
-[23]: https://getbootstrap.com/docs/4.0/components/forms/#checkboxes-and-radios-1
-[24]: https://getbootstrap.com/docs/4.0/components/forms/#select-menu
-[25]: https://getbootstrap.com/docs/4.0/components/forms/#file-browser
